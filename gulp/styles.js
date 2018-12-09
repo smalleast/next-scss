9f117e41adcb6a108552a85aca8e0f943e31738c
smalleast <382026180@qq.com>
2018-12-09 10:54:53 +0800

    add next-scss

---
+++ gulp/styles.js
@@ -0,0 +1,24 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //styles
+  gulp.task('styles',function() {
+    return gulp.src([
+      'src/_flex-layout.scss',
+      'src/_scss-mixins.scss',
+      'src/_colors.scss',
+      'src/_generators.scss'
+    ])
+      .pipe($.concat('next-scss.scss'))
+      .pipe(gulp.dest('dist'));
+  });
+
+}());
